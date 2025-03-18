4d41c7f720563b1ba963bc553b06fe8d663d1bf9
Zach <zach@mymanagr.com>
2025-03-17 23:45:03 -0400

    fixed issue with pydantic types

--- server/remindme/schemas/list_schemas.py
+++ server/remindme/schemas/list_schemas.py
@@ -1,24 +1,25 @@
 from pydantic import BaseModel
+from typing import List
 from uuid import UUID
 
 class ItemBase(BaseModel):
-    name = str
-    quantity = int
+    name: str
+    quantity: int
 
 class ItemCreate(ItemBase):
-    checked = bool
-    list_id = UUID
+    checked: bool
+    list_id: UUID
 
 class ClientItem(ItemCreate):
-    id = UUID
+    id: UUID
 
 class ListBase(BaseModel):
-    name = str
-    store = str
+    name: str
+    store: str
 
 class ListCreate(ListBase):
-    items = [ItemBase]
+    items: List[ItemBase]
     user_id: UUID    
 
 class ClientList(ListCreate):
-    id = UUID
+    id: UUID
